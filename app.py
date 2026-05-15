fda48e084e83e88341bd44a328b4160c564c8829
SLENOUO <stephan.lenouo@groupe-esigelec.org>
2026-05-15 23:13:26 +0200

    Fix port Railway

--- app.py
+++ app.py
@@ -488,4 +488,5 @@ scheduler.start()
 if __name__ == "__main__":
     print("\n🚀 JobAgent démarré → http://localhost:5000")
     print("⏰ Scan automatique programmé chaque matin à 8h00\n")
-    app.run(debug=False, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+app.run(debug=False, host="0.0.0.0", port=port)
